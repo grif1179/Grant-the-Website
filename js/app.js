1b9eef19fad5668964524c29c53419aadb96aaa6
Grant Park <grif1179@gmail.com>
2016-05-31 17:01:19 -0400

    added mobile sizing changes

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
-$(".side_menu button").hover(
-function(){
+$(".side_menu button").hover(function(){
   $(this).filter(':not(:animated)').animate({
      marginLeft:'0em'
   },'slow');
@@ -13,13 +12,35 @@ function() {
 
 $(".imgWrap").hover(function(){
     
-        $(this).filter(':not(:animated)').animate({opacity: 0.50},'fast');
-        $(this).find(".textWrap").stop().show("fast");
+        $(this).css("opacity", 0.50);
+        $(this).find(".textWrap").css("display","block");
         },
      function(){
         
-        $(this).animate({opacity: 100},'fast');
-        $(this).find(".textWrap").stop().hide('fast');    
+        $(this).css("opacity", 100);
+        $(this).find(".textWrap").css("display","none");    
 });
 
 
+$("#drop_down").click(function(){
+    if($(this).attr("src")=="img/location-arrow-icon-icon-ios7-arrow-down-128.png"){
+        $(this).attr("src","img/drop_down_left.png");
+        $(".drop_menu").animate({right:"0em"},"fast");
+//        $(".content").animate({marginRight:"10em"},"fast");
+    }else{
+        $(this).attr("src","img/location-arrow-icon-icon-ios7-arrow-down-128.png");
+        $(".drop_menu").animate({right:"-10em"},"fast");
+//      $(".content").animate({marginRight:"0em"},"fast");
+    }
+});    
+    
+$(".content").click(function(){
+    if($("#drop_down").attr("src")=="img/drop_down_left.png"){
+        $("#drop_down").attr("src","img/location-arrow-icon-icon-ios7-arrow-down-128.png");
+        $(".drop_menu").animate({right:"-10em"},"fast");
+    }
+});    
+
+$(function(){
+    $('input').focus();
+});
